ffe48ae37a913c5ea60f6e18af5518b0de7d16e0
Matthias Van Duysen <matthias@vanduysen.be>
2026-02-17 18:32:27 +0100

    cleanup

--- calc/rendabiliteit/rendabiliteit.py
+++
@@ -1,8 +0,0 @@
-from verkopen import brutoverkoopmarge, nettoverkoopmarge
-
-a = float(input("a"))
-b = float(input("b"))
-
-print(f"De brutoverkoopmarge bedraagt {brutoverkoopmarge(a,b)}.")
-print(f"De nettoverkoopmarge bedraagt {nettoverkoopmarge(a,b)}.")
-
