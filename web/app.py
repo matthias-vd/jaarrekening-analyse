1be5f87d8b3ab55d6918a8659bb0e9dd1d65ecfe
Matthias Van Duysen <matthias@vanduysen.be>
2026-02-20 16:12:34 +0100

    add flask website

---
+++ web/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+from core.StructuurBalans import structuurBalans
+from core.StructuurMetadata import structuurMetadata
+from core.StructuurResultatenRekening import structuurResultatenRekening
+from core.StructuurSocialeBalans import structuurSocialeBalans
+from core.StructuurToelichting import structuurToelichting
+app = Flask(__name__)
+@app.route('/structuren', methods=['GET', 'POST'])
+
+def login():
+    if request.method == 'POST':
+        structuren = request.form['structuur']
+        if structuren == 'Balans':
+            return structuurBalans
+        elif structuren == 'SocialeBalans':
+            return structuurSocialeBalans
+        elif structuren == 'ResultatenRekening':
+            return structuurResultatenRekening
+        elif structuren == 'Toelichting':
+            return structuurToelichting
+        else:
+            return "omhooggevallen frietketel, wat je hebt ingetypt is geen optie"
+    return render_template('structuren.html')
+
+if __name__ == '__main__':
+    app.run()
